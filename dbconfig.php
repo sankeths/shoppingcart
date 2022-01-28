3f6e67b0ae029186f60406fcab8960013dc1ab23
sankeths <s.sanketh@gmail.com>
2022-01-28 23:17:12 +0530

    first commit

---
+++ dbconfig.php
@@ -0,0 +1,7 @@
+<?php
+   define('DB_SERVER', 'localhost');
+   define('DB_USERNAME', 'sanketh');
+   define('DB_PASSWORD', 'India@123');
+   define('DB_DATABASE', 'cartapplication');
+   $db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+?>
